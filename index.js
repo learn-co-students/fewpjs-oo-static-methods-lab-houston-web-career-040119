44f736bc77e9fb8d93b4f1b214210922b058100e
shawn0527 <shawnren527@gmail.com>
2019-05-20 21:53:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9\s'-]/g, '')
+  }
+
+  static titleize(string) {
+    let array = []
+    string.split(' ').forEach((word) => {
+      if(string.split(' ').indexOf(word) == 0) {
+        word = this.capitalize(word)
+      } else if(word !== 'the' && word !== 'of' && word !== 'and' && word !== 'a' && word !== 'an' && word !== 'but' && word !== 'for' && word !== 'at' && word !== 'by' && word !== 'from') {
+        word = this.capitalize(word)
+      }
+      array.push(word)
+    })
+
+    return array.join(' ')
+  }
 }
